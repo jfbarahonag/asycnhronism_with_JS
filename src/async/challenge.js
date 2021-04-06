9e6df9f009b25cba6c24e2ff78cc239e6519f52f
jfbarahonag <felipe.barahona@titoma.com>
2021-04-06 15:23:49 -0500

    Some variables were renamed

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -3,13 +3,23 @@ const API = 'https://rickandmortyapi.com/api/character/';
 
 const anotherFunction = async (url_api) => {
     try {
+        console.time();
+
         const data = await fetchData(url_api);
-        const character = await fetchData(`${url_api}${data.results[0].id}`);
+        const { count } = data.info;
+        const { id } = data.results[0];
+
+        const character = await fetchData(`${url_api}${id}`);
+        const { name } = character;
+
         const origin = await fetchData(character.origin.url);
+        const { dimension } = origin;
         
-        console.log(data.info.count);
-        console.log(character.name);
-        console.log(origin.dimension);
+        console.timeEnd()
+        
+        console.log(count);
+        console.log(name);
+        console.log(dimension);
     } catch (error) {
         console.error(error);
     }
